9596a9fed599a2833972fb53df0627d69958943a
leopold <vdduong14@gmail.com>
2026-08-29 17:44:52 +0700

    feat(web): add assignments action

--- apps/web/app/libs/actions.ts
+++ apps/web/app/libs/actions.ts
@@ -4,6 +4,10 @@ export const INITIAL_ACTION_STATUS = { message: "", status: ACTION_STATUS.NORMAL
 
 export enum ACTION_TAG {
 	/* dashboard */
+	CREATE_ASSIGNMENT = "createAssignment",
+	DELETE_ASSIGNMENT = "deleteAssignment",
+	GET_ASSIGNMENT = "getAssignment",
+	GET_MANY_ASSIGNMENTS = "getManyAssignments",
 	GET_MANY_SHIPMENTS = "getManyShipments",
 	UPDATE_SHIPMENT = "updateShipment",
 }
